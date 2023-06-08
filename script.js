624a2f2049762c058fb9259c6047a76750433aa3
Султан <carameleclipsimus@gmail.com>
2023-06-08 11:44:30 +0600

    2 is done

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+const birdsPerDay = [
+  4, 7, 3, 2, 1, 1, 2, 0, 2, 3, 2, 7, 1, 3, 0, 6, 5, 3, 7, 2, 3,
+];
 
-function totalBirdCount(arr) {
+function birdsInWeek(birdsPerDay, week) {
   let temp = 0;
-  for (let i = 0; i < arr.length; i++) {
-    temp += arr[i];
+  let counter = (week - 1) * 7;
+  if (week === 0 || week === 1) counter = 0;
+  for (let i = counter; i < week * 7; i++) {
+    temp += birdsPerDay[i];
   }
   return temp;
 }
+console.log(birdsInWeek(birdsPerDay, 2));
+// function totalBirdCount(birdsPerDay) {
+//   let temp = 0;
+//   for (let i = 0; i < birdsPerDay.length; i++) {
+//     temp += birdsPerDay[i];
+//   }
+//   return temp;
+// }
 
-console.log(totalBirdCount(birdsPerDay));
+// console.log(totalBirdCount(birdsPerDay));
